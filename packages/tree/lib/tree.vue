4f2a3cf671adfe1515cecb23559cf9a888c869bb
zengchaowu <root@zengchaowu.com>
2022-07-07 16:19:12 +0800

    chore: tree

---
+++ packages/tree/lib/tree.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="flex flex-col">
+    <button>
+      <img class="w-5 h-5" :src="payload.icon" />
+      <span v-if="payload.name" class="min-w-0 truncate">
+        {{ payload.name }}
+      </span>
+    </button>
+    <template v-if="payload.children" class="flex flex-col">
+      <DoraemonTree
+        v-for="(child, index) in payload.children"
+        :key="index"
+        :payload="child"
+        :indent="indent + 1"
+      />
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DoraemonTree",
+  props: {
+    payload: {
+      type: Object,
+      default: undefined,
+    },
+    indent: {
+      type: Number,
+      default: 0,
+    },
+  },
+};
+</script>
