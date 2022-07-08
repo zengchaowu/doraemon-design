31aa9c2768fcef480486a7f69abfb9884028bd06
zengchaowu <root@zengchaowu.com>
2022-07-08 11:15:06 +0800

    chore: 完善静态tree

--- packages/tree/lib/tree.vue
+++ packages/tree/lib/tree.vue
@@ -1,12 +1,61 @@
 <template>
-  <div class="flex flex-col">
-    <button v-if="payload.icon || payload.title">
-      <img class="w-5 h-5" :src="payload.icon" />
-      <span v-if="payload.title" class="min-w-0 truncate">
-        {{ payload.title }}
-      </span>
-    </button>
-    <template v-if="payload.children" class="flex flex-col">
+  <div
+    id="container"
+    class="flex flex-col"
+    :style="{
+      gap: '1px',
+    }"
+  >
+    <div
+      v-if="payload.icon || payload.title"
+      id="link"
+      class="flex justify-between items-center"
+      :style="{
+        padding: '8px 16px',
+        cursor: payload.path ? 'pointer' : 'default',
+        pointerEvents: payload.path || payload.children ? 'default' : 'none',
+      }"
+      @click="jump"
+    >
+      <div
+        class="flex-grow flex items-center"
+        :style="{
+          paddingLeft: (indent - 1) * 26 + 'px',
+          gap: '8px',
+        }"
+      >
+        <img
+          v-if="indent < 2"
+          :src="'/icons' + payload.path + '.svg'"
+          :style="{
+            width: '18px',
+            height: '18px',
+          }"
+        />
+        <span v-if="payload.title" id="title" class="min-w-0 truncate">
+          {{ payload.title }}
+        </span>
+      </div>
+      <div
+        v-if="payload.children"
+        id="arrow"
+        class="flex items-center"
+        :style="{
+          padding: '4px',
+          transform: payload.isOpen ? 'rotate(90deg)' : undefined,
+        }"
+        @click="open"
+      >
+        <img
+          src="/icons/arrow.svg"
+          :style="{
+            width: '18px',
+            height: '18px',
+          }"
+        />
+      </div>
+    </div>
+    <template v-if="payload.children && payload.isOpen">
       <DoraemonTree
         v-for="(child, index) in payload.children"
         :key="index"
@@ -28,6 +77,16 @@ export default {
     indent: {
       type: Number,
       default: 0,
+    },
+  },
+  methods: {
+    open() {
+      this.payload.isOpen = !this.payload.isOpen;
+    },
+    jump() {
+      if (this.payload.children) {
+        this.open();
+      }
     },
   },
 };
