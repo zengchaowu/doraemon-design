8a77176b48c5cc89746a0ba1bc93ec3883992824
zengchaowu <root@zengchaowu.com>
2022-07-09 18:48:57 +0800

    chore: add lottie

---
+++ packages/lottie/lib/Lottie.vue
@@ -0,0 +1,24 @@
+<template>
+  <div ref="lottie"></div>
+</template>
+
+<script>
+import lottie from "lottie-web";
+export default {
+  props: {
+    payload: {
+      type: Object,
+      default: undefined,
+    },
+  },
+  mounted() {
+    lottie.loadAnimation({
+      container: this.$refs.lottie,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      path: this.payload.path,
+    });
+  },
+};
+</script>
