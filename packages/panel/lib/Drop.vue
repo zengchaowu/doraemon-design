6754775576b10288eca6560a2e7ddb3f2c06ec40
zengchaowu <root@zengchaowu.com>
2022-07-17 15:53:18 +0800

    chore: add drop

---
+++ packages/panel/lib/Drop.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    @drop.prevent="onDrop"
+    @dragover.prevent="onDragover"
+    @dragleave.prevent="onDragleave"
+  >
+    <div class="pointer-events-none">
+      <slot />
+    </div>
+    <div
+      v-if="dragover"
+      class="absolute inset-0 bg-gray-900 bg-opacity-50 pointer-events-none"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dragover: false,
+    };
+  },
+  methods: {
+    onDragover() {
+      this.dragover = true;
+    },
+    onDragleave() {
+      this.dragover = false;
+    },
+    onDrop(e) {
+      this.dragover = false;
+      console.log(e.dataTransfer.files);
+    },
+  },
+};
+</script>
