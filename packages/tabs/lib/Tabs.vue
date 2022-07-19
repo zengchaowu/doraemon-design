5b3ef5c290b47b87942d1652fb5a3f76b32544f5
zengchaowu <root@zengchaowu.com>
2022-07-19 09:38:33 +0800

    chore: stash

---
+++ packages/tabs/lib/Tabs.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex gap-4">
+    <div
+      v-for="(tab, index) in payload?.tabs"
+      :key="index"
+      class="font-medium cursor-pointer"
+      :class="tab.value === value ? 'text-primary' : 'text-gray-500'"
+      @click="$emit('update', tab.value)"
+    >
+      <span>{{ tab.label }}</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  model: {
+    prop: "value",
+    event: "update",
+  },
+  props: {
+    value: {
+      type: [String, Number],
+      default: undefined,
+    },
+    payload: {
+      type: Object,
+      default: undefined,
+    },
+  },
+};
+</script>
