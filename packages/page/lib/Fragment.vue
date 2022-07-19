846eb85d69af97fc3cc4184c73d2484daf70ed02
zengchaowu <root@zengchaowu.com>
2022-07-19 18:02:13 +0800

    chore: stash

--- packages/page/lib/Fragment.vue
+++ packages/page/lib/Fragment.vue
@@ -1,20 +1,20 @@
 <template>
   <div ref="container" class="absolute inset-0 flex">
-    <template v-if="host">
+    <template v-if="payload">
       <template v-if="context === 'load'">
         <template v-if="state.load === 'success'">
           <template v-if="data.load.length > 0">
             <slot name="load" v-bind:context="{ state, data }"></slot>
           </template>
           <template v-else>
-            <component :is="host.components[context]?.empty" />
+            <component :is="payload.components[context]?.empty" />
           </template>
         </template>
         <template v-else-if="state.load === 'reloading'">
-          <component :is="host.components[context]?.reloading" />
+          <component :is="payload.components[context]?.reloading" />
         </template>
         <template v-else-if="state.load === 'reloadError'">
-          <component :is="host.components[context]?.reloadError" />
+          <component :is="payload.components[context]?.reloadError" />
         </template>
       </template>
       <template v-else-if="context === 'search'">
@@ -30,7 +30,7 @@
 <script>
 export default {
   props: {
-    host: {
+    payload: {
       type: Object,
       default: undefined,
     },
@@ -90,32 +90,34 @@ export default {
       },
       methods: {
         load: {
-          async reloading() {
+          reloading: async () => {
             try {
               this.context = "load";
               this.state.load = "reloading";
-              const result = await host.requestData();
+              const result = await payload.methods.requestData();
               this.data.load = result;
               this.state.load = "success";
             } catch (error) {
-              host.methods.handleError && host.methods.handleError(error);
+              this.payload?.methods?.handleError &&
+                this.payload.methods.handleError(error);
             }
           },
-          async appending() {
+          appending: async () => {
             try {
               this.context = "load";
               this.state.load = "appending";
-              const result = await host.requestData();
+              const result = await payload.methods.requestData();
               this.data.load = this.data.load.concat(result);
               this.state.load = "success";
             } catch (error) {
-              host.methods.handleError && host.methods.handleError(error);
+              this.payload?.methods?.handleError &&
+                this.payload.methods.handleError(error);
             }
           },
         },
         search: {
-          reloading() {},
-          appending() {},
+          reloading: () => {},
+          appending: () => {},
         },
       },
     };
