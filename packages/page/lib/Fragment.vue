3803ad32f885563e83b5127a5e92f96fa3e44269
zengchaowu <root@zengchaowu.com>
2022-07-19 17:42:27 +0800

    chore: stash

--- packages/page/lib/Fragment.vue
+++ packages/page/lib/Fragment.vue
@@ -1,19 +1,40 @@
 <template>
   <div ref="container" class="absolute inset-0 flex">
-    <template v-if="context === 'load'">
-      <slot name="load" v-bind:context="{ state, data }"></slot>
-    </template>
-    <template v-else-if="context === 'search'">
-      <slot name="search" v-bind:context="{ state, data }"></slot>
-    </template>
-    <template v-else>
-      <slot v-bind:context="{ state, data }"></slot>
+    <template v-if="host">
+      <template v-if="context === 'load'">
+        <template v-if="state.load === 'success'">
+          <template v-if="data.load.length > 0">
+            <slot name="load" v-bind:context="{ state, data }"></slot>
+          </template>
+          <template v-else>
+            <component :is="host.components[context]?.empty" />
+          </template>
+        </template>
+        <template v-else-if="state.load === 'reloading'">
+          <component :is="host.components[context]?.reloading" />
+        </template>
+        <template v-else-if="state.load === 'reloadError'">
+          <component :is="host.components[context]?.reloadError" />
+        </template>
+      </template>
+      <template v-else-if="context === 'search'">
+        <slot name="search" v-bind:context="{ state, data }"></slot>
+      </template>
+      <template v-else>
+        <slot v-bind:context="{ state, data }"></slot>
+      </template>
     </template>
   </div>
 </template>
 
 <script>
 export default {
+  props: {
+    host: {
+      type: Object,
+      default: undefined,
+    },
+  },
   data() {
     return {
       context: null, // null load search
@@ -51,11 +72,53 @@ export default {
         load: undefined,
         search: undefined,
       },
+      components: {
+        load: {
+          empty: undefined,
+          reloading: undefined,
+          appending: undefined,
+          reloadError: undefined,
+          appendError: undefined,
+        },
+        search: {
+          empty: undefined,
+          reloading: undefined,
+          appending: undefined,
+          reloadError: undefined,
+          appendError: undefined,
+        },
+      },
+      methods: {
+        load: {
+          async reloading() {
+            try {
+              this.context = "load";
+              this.state.load = "reloading";
+              const result = await host.requestData();
+              this.data.load = result;
+              this.state.load = "success";
+            } catch (error) {
+              handleError(error);
+            }
+          },
+          async appending() {
+            try {
+              this.context = "load";
+              this.state.load = "appending";
+              const result = await host.requestData();
+              this.data.load = this.data.load.concat(result);
+              this.state.load = "success";
+            } catch (error) {
+              handleError(error);
+            }
+          },
+        },
+        search: {
+          reloading() {},
+          appending() {},
+        },
+      },
     };
-  },
-  methods: {
-    reloadData() {},
-    appendData() {},
   },
 };
 </script>
