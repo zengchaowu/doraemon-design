70c296f6573efabd36b96b58c6290c6eb901c550
zengchaowu <root@zengchaowu.com>
2022-07-20 22:23:11 +0800

    chore: stash

---
+++ packages/input/lib/LazySelect.vue
@@ -0,0 +1,71 @@
+<template>
+  <div v-if="preview" class="flex truncate items-center min-w-0">
+    <a-tooltip class="min-w-0 truncate">
+      <template #title>
+        {{ parseSelect(payload?.options, value) }}
+      </template>
+      {{ parseSelect(payload?.options, value) }}
+    </a-tooltip>
+  </div>
+  <a-select
+    v-else
+    class="w-32"
+    :value="value"
+    :placeholder="payload?.placeholder ?? '请选择' + payload?.label"
+    :disabled="disabled"
+    @click.native="click"
+    @change="(option) => $emit('update', option)"
+  >
+    <div slot="dropdownRender" slot-scope="menu">
+      <div v-if="state === 'success'" class="content">
+        <a-spin />
+      </div>
+      <div v-else-if="state === 'fail'" class="content">
+        <span>加载失败</span>
+      </div>
+      <div v-else class="content">
+        <a-spin />
+      </div>
+    </div>
+  </a-select>
+</template>
+
+<script>
+import parseSelect from "../functions/parseSelect.js";
+import _interface from "../mixin/input.js";
+export default {
+  mixins: [_interface],
+  data() {
+    return {
+      state: undefined, // loading success fail
+      options: undefined,
+    };
+  },
+  methods: {
+    parseSelect,
+    async click() {
+      console.log(this.state);
+      if (this.state === undefined || this.state === "fail") {
+        this.state = "loading";
+        try {
+          const result = await this.payload?.getOptions();
+          this.options = result;
+          this.state = "success";
+        } catch (error) {
+          console.log(error);
+          this.state = "fail";
+        }
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+}
+</style>
