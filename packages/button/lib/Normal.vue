df04f36f4192b11d8c0039750990bc5097eeb300
zengchaowu <root@zengchaowu.com>
2022-07-29 10:17:55 +0800

    chore: stash

---
+++ packages/button/lib/Normal.vue
@@ -0,0 +1,18 @@
+<template>
+  <a-button class="flex items-center" @click="payload.onClick?.call()">
+    <span>
+      {{ payload?.label }}
+    </span>
+  </a-button>
+</template>
+
+<script>
+export default {
+  props: {
+    payload: {
+      type: Object,
+      default: undefined,
+    },
+  },
+};
+</script>
