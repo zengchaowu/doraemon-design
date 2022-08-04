32213cd08864c4af95ab72bab9a66cc9d58d0e9c
zengchaowu <root@zengchaowu.com>
2022-08-04 14:55:39 +0800

    chore: stash

---
+++ packages/button/lib/Icon.vue
@@ -0,0 +1,28 @@
+<template>
+  <a-dropdown v-if="payload?.subType === 'dropdown'">
+    <button type="button">
+      <div
+        class="w-5 h-5 fill-current"
+        v-html="require(`~/assets/images/icons/${payload?.image}?include`)"
+      />
+      <component v-if="payload?.overlay" :is="payload.overlay" />
+    </button>
+  </a-dropdown>
+  <button type="button" @click="payload?.onClick?.call()">
+    <div
+      class="w-5 h-5 fill-current"
+      v-html="require(`~/assets/images/icons/${payload?.image}?include`)"
+    />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    payload: {
+      type: Object,
+      default: undefined,
+    },
+  },
+};
+</script>
