fe922c789bf0704ac18af90a50a3de1fd2307dce
zengchaowu <root@zengchaowu.com>
2022-08-10 09:12:39 +0800

    chore: stash

---
+++ packages/input/lib/Week.vue
@@ -0,0 +1,31 @@
+<template>
+  <div v-if="preview" class="flex truncate items-center min-w-0 h-8">
+    <a-tooltip class="min-w-0 truncate">
+      <template #title>
+        {{ value }}
+      </template>
+      {{ value }}
+    </a-tooltip>
+  </div>
+  <a-date-picker
+    v-else
+    :value="value"
+    :disabled="disabled"
+    :placeholder="payload?.placeholder ?? '请选择' + payload?.label"
+    format="YYYY-MM-DD"
+    style="height: fit-content"
+    @change="change"
+  />
+</template>
+
+<script>
+import _interface from "../mixin/input.js";
+export default {
+  mixins: [_interface],
+  methods: {
+    change(_, value) {
+      this.$emit("update", value);
+    },
+  },
+};
+</script>
