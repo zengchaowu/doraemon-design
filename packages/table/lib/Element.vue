6062851cd7fde1e2b16375df1c020e432172a4d8
zengchaowu <root@zengchaowu.com>
2022-08-11 21:59:53 +0800

    chore: stash

--- packages/table/lib/Element.vue
+++ packages/table/lib/Element.vue
@@ -2,51 +2,53 @@
   <DoraemonFragment ref="fragment" :payload="{ components, request }">
     <template #load="{ context }">
       <el-table :data="context.data" height="100%" @row-click="handleRowClick">
-        <el-table-column
-          v-for="(column, index) in columns"
-          :key="index"
-          class="flex-grow basis-1"
-          :min-width="column.width"
-          :fixed="column.fixed"
-        >
-          <template slot="header">
-            <div :class="appearance?.header">
-              <span class="min-w-0 truncate"> {{ column.label }} </span>
-            </div>
-          </template>
-          <template slot-scope="scope">
-            <div :class="appearance?.content">
-              <a-tooltip
-                v-if="column.type === 'text'"
-                :class="appearance?.contentText"
-              >
-                <template #title>
+        <template v-for="(column, index) in columns">
+          <el-table-column
+            v-if="column?.visible !== false"
+            :key="index"
+            class="flex-grow basis-1"
+            :min-width="column.width"
+            :fixed="column.fixed"
+          >
+            <template slot="header">
+              <div :class="appearance?.header">
+                <span class="min-w-0 truncate"> {{ column.label }} </span>
+              </div>
+            </template>
+            <template slot-scope="scope">
+              <div :class="appearance?.content">
+                <a-tooltip
+                  v-if="column.type === 'text'"
+                  :class="appearance?.contentText"
+                >
+                  <template #title>
+                    <div
+                      class="whitespace-pre-wrap"
+                      v-html="
+                        column.display
+                          ? column.display(get(scope.row, column.value))
+                          : get(scope.row, column.value)
+                      "
+                    />
+                  </template>
                   <div
-                    class="whitespace-pre-wrap"
+                    class="whitespace-pre-wrap line-clamp-2"
                     v-html="
                       column.display
                         ? column.display(get(scope.row, column.value))
                         : get(scope.row, column.value)
                     "
                   />
-                </template>
-                <div
-                  class="whitespace-pre-wrap line-clamp-2"
-                  v-html="
-                    column.display
-                      ? column.display(get(scope.row, column.value))
-                      : get(scope.row, column.value)
-                  "
+                </a-tooltip>
+                <component
+                  v-else-if="column.type === 'custom'"
+                  :is="column.component"
+                  :payload="column.payload(scope.row)"
                 />
-              </a-tooltip>
-              <component
-                v-else-if="column.type === 'custom'"
-                :is="column.component"
-                :payload="column.payload(scope.row)"
-              />
-            </div>
-          </template>
-        </el-table-column>
+              </div>
+            </template>
+          </el-table-column>
+        </template>
       </el-table>
     </template>
   </DoraemonFragment>
