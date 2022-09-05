bdddb7bc237c2cf6850bcdf22ad8bf406ab6966d
zengchaowu <root@zengchaowu.com>
2022-09-05 10:12:30 +0800

    chore: stash

---
+++ packages/page/lib/Loading.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="relative">
+    <slot />
+    <TheStateLoading
+      v-if="isLoading"
+      class="absolute inset-0 bg-light1 bg-opacity-30"
+    />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    isLoading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
