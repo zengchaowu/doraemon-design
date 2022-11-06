16285511fa2a8cce6708e36478bb7136576e6914
zengchaowu <root@zengchaowu.com>
2022-11-07 00:31:56 +0800

    chore: stash

--- packages/button/lib/Icon.vue
+++ packages/button/lib/Icon.vue
@@ -17,7 +17,7 @@
   <button
     v-else
     type="button"
-    class="flex items-center gap-1"
+    class="flex items-center gap-1 relative"
     @click.stop="payload?.onClick?.call()"
   >
     <div
@@ -27,6 +27,10 @@
     />
     <img v-if="payload?.src" class="w-5 h-5" :src="payload.src" />
     <span v-if="payload?.label">{{ payload.label }}</span>
+    <div
+      v-if="payload?.badge"
+      class="absolute top-0 right-0 w-2 h-2 bg-red-500 rounded-full"
+    ></div>
   </button>
 </template>
 
