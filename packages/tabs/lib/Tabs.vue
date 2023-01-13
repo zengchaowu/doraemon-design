3414234e5a00825a5f17bb52779b533bd8248954
zengchaowu <root@zengchaowu.com>
2023-01-13 16:04:13 +0800

    chore: tabs支持红点

--- packages/tabs/lib/Tabs.vue
+++ packages/tabs/lib/Tabs.vue
@@ -4,10 +4,16 @@
       v-for="(tab, index) in payload?.tabs"
       :key="index"
       type="button"
+      class="relative"
       :class="tab.value === value ? 'text-primary' : 'text-gray-500'"
       @click="$emit('update', tab.value)"
     >
       <span class="font-semibold">{{ tab.label }}</span>
+      <span
+        v-if="tab?.unreadCount > 0"
+        class="absolute -right-1 -top-1 flex-none w-2 h-2 rounded-full"
+        style="background-color: red"
+      />
     </button>
   </div>
 </template>
